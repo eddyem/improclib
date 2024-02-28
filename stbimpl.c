34de5183c9521c263c76e4d4460fed901cb08b23
Edward Emelianov <eddy@sao.ru>
2024-02-28 17:39:11 +0300

    err: <> instead of ""

--- stbimpl.c
+++ stbimpl.c
@@ -24,6 +24,6 @@
 #define STBI_NO_PNM
 
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
+#include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb/stb_image_write.h>
+#include "stb/stb_image_write.h"
